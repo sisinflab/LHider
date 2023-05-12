dbec00769e1e99d18c02fec634422e1915467a17
alberto <alberto.mancino94@gmail.com>
2023-05-12 19:29:42 +0200

    best model fixed

--- best_model.py
+++ best_model.py
@@ -1,18 +1,39 @@
+import glob
 import json
-import os
 import argparse
 from config_templates.best import TEMPLATE
 from elliot.run import run_experiment
+from src.loader.paths import *
 
 config_dir = 'config_files/'
 RANDOM_SEED = 42
 metrics = ["nDCGRendle2020", "Recall", "HR", "nDCG", "Precision", "F1", "MAP", "MAR", "ItemCoverage", "Gini",
-           "SEntropy", "EFD", "EPC",  "PopREO", "PopRSP", "ACLT", "APLT", "ARP"]
+           "SEntropy", "EFD", "EPC", "PopREO", "PopRSP", "ACLT", "APLT", "ARP"]
 
 
-def run_best(args: argparse.Namespace):
-    dataset_name = args.dataset
-    best_json = os.path.join("results", dataset_name, "performance", args.json)
+def dataset_from_performance_file(file: str):
+    """
+    Given the dataset name and the path of the json file containing the best model parameters returns the specif
+    dataset name
+    @param dataset_name: base name of the dataset
+    @param file: path of the best configuration json file
+    @return: string containing the name of the dataset related to the best model parameters file
+    """
+    #dataset = [token for token in os.path.normpath(file).split(os.sep) if token.startswith(dataset_name)][0]
+    return os.path.normpath(file).split(os.sep)[-3]
+
+
+def find_best_performance_files(dataset_name: str):
+    path_format_string = os.path.join(RESULT_DIR,
+                                      f'{dataset_name}*',
+                                      'performance',
+                                      'bestmodelparams_*.json')
+    json_performance_files = glob.glob(path_format_string)
+    return [{'dataset_name': dataset_from_performance_file(file=j), 'best_json': j}
+            for j in json_performance_files]
+
+
+def run_best(dataset_name: str, best_json: str):
 
     if not os.path.exists(best_json):
         FileNotFoundError(f'File not found at {best_json}. Please, check your files.')
@@ -43,7 +64,9 @@ def run_best(args: argparse.Namespace):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", required=True, type=str)
-parser.add_argument("--json", required=True, type=str)
+#parser.add_argument("--json", required=True, type=str)
 
 args = parser.parse_args()
-run_best(args)
+
+for arg in find_best_performance_files(dataset_name=args.dataset):
+    run_best(**arg)
---
+++ compute_recommendation.py
@@ -0,0 +1,28 @@
+import glob
+import argparse
+from config_templates.training import TEMPLATE
+from src.loader.paths import *
+from elliot.run import run_experiment
+
+config_dir = 'config_files/'
+RANDOM_SEED = 42
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--dataset', required=True, type=str)
+parser.add_argument('--all', action='store_true')
+args = parser.parse_args()
+
+
+if args.all:
+    dataset_list = [os.path.basename(absolute_path)
+                    for absolute_path in glob.glob(os.path.join(RESULT_DIR, f'{args.dataset}*'))]
+else:
+    dataset_list = [f'{args.dataset}']
+
+for dataset_name in dataset_list:
+    config = TEMPLATE.format(dataset=dataset_name)
+    config_path = os.path.join(config_dir, 'runtime_conf.yml')
+    with open(config_path, 'w') as file:
+        file.write(config)
+    run_experiment(config_path)
