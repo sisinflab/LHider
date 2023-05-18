173191bde471958fd37797f0c3341bfb9d81a40c
a-difazio <angela_933@hotmail.it>
2023-05-18 18:12:05 +0200

    added script to run other script in sequence

--- compute_recommendation.py
+++ compute_recommendation.py
@@ -1,4 +1,3 @@
-import glob
 import argparse
 from config_templates.training import TEMPLATE
 from src.loader.paths import *
@@ -10,19 +9,12 @@ RANDOM_SEED = 42
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--dataset', required=True, type=str)
-parser.add_argument('--all', action='store_true')
 args = parser.parse_args()
 
+dataset_name = args.dataset
 
-if args.all:
-    dataset_list = [os.path.basename(absolute_path)
-                    for absolute_path in glob.glob(os.path.join(RESULT_DIR, f'{args.dataset}*'))]
-else:
-    dataset_list = [f'{args.dataset}']
-
-for dataset_name in dataset_list:
-    config = TEMPLATE.format(dataset=dataset_name)
-    config_path = os.path.join(config_dir, 'runtime_conf.yml')
-    with open(config_path, 'w') as file:
-        file.write(config)
-    run_experiment(config_path)
+config = TEMPLATE.format(dataset=dataset_name)
+config_path = os.path.join(config_dir, 'runtime_conf.yml')
+with open(config_path, 'w') as file:
+    file.write(config)
+run_experiment(config_path)
