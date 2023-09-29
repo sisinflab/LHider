e43db1f1cd2444e6629121ae42079491c0e8d432
alberto <alberto.mancino94@gmail.com>
2023-09-29 15:28:02 +0200

    project path fix

--- src/loader/paths.py
+++ src/loader/paths.py
@@ -1,6 +1,6 @@
 import os
 
-PROJECT_PATH = os.path.abspath('.')
+PROJECT_PATH = os.path.abspath('..')
 DATA_DIR = os.path.join(PROJECT_PATH, 'data')
 RESULT_DIR = os.path.join(PROJECT_PATH, 'results')
 GENERATED_DIR = os.path.join(PROJECT_PATH, 'generated')
