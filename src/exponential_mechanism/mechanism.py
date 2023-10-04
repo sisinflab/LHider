overflow fix

src/exponential_mechanism/mechanism.py
@@ -14,7 +14,8 @@ class ExponentialMechanism:
         return np.array([self.score_function(x) for x in output])
 
     def probabilities(self, output):
-        probabilities = np.exp(np.float128(self.eps * self.scores(output) / (2 * self.sensitivity)))
+        exponent = (self.eps * self.scores(output)) / (2 * self.sensitivity)
+        probabilities = np.exp(exponent.astype('float128'))
         probabilities = probabilities / np.sum(probabilities)
         return probabilities
 