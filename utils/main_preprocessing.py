36dad60811a5d711e9adf00806ea5328feeed563
a-difazio <angela_933@hotmail.it>
2023-10-05 11:52:16 +0200

    added binarization

--- utils/main_preprocessing.py
+++ utils/main_preprocessing.py
@@ -1,6 +1,5 @@
 from data_preprocessing.data_preprocessing import run
 
-
 if __name__ == '__main__':
     run(dataset_name='facebook_books', core=5)
     run(dataset_name='yahoo_movies', core=10, threshold=3)
