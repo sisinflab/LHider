3129323d6eff5519c9f3cfeba2902d2a34e2334e
alberto <alberto.mancino94@gmail.com>
2024-01-17 14:35:32 +0100

    manhattan

--- main_sigir4.py
+++ main_sigir4.py
@@ -2,7 +2,7 @@ from src.jobs.sigir import run_new_expo
 
 # definisci i parametri necessari
 randomizer = 'randomized'
-exp_score = 'jaccard'
+exp_score = 'manhattan'
 
 # dataset
 dataset_name = 'yahoo_movies'
