c6cb502b7b8bf06420cd62f3943452574f570ef2
albertomancino <alberto.mancino94@gmail.com>
2024-01-23 19:41:01 +0100

    config vision

--- main_recs.py
+++ main_recs.py
@@ -3,7 +3,7 @@ from recs import run as run_recs
 dataset_name = 'yahoo_movies'
 dataset_type = 'train'
 
-for base_seed in range(400, 501, 100):
+for base_seed in range(2200, 3601, 100):
     args = {
         'dataset_name': dataset_name,
         'dataset_type': dataset_type,
