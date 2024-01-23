245ceb0fabd4e1943bf9d95483a5f89e8a2a3887
albertomancino <alberto.mancino94@gmail.com>
2024-01-23 19:44:32 +0100

    mail recs

--- main_recs.py
+++ main_recs.py
@@ -3,10 +3,18 @@ from recs import run as run_recs
 dataset_name = 'yahoo_movies'
 dataset_type = 'train'
 
-for base_seed in range(2200, 3601, 100):
-    args = {
-        'dataset_name': dataset_name,
-        'dataset_type': dataset_type,
-        'base_seed': base_seed
-    }
-    run_recs(args)
+
+def fun():
+    for base_seed in range(2200, 3601, 100):
+        args = {
+            'dataset_name': dataset_name,
+            'dataset_type': dataset_type,
+            'base_seed': base_seed
+        }
+        run_recs(args)
+
+
+from email_notifier.email_sender import EmailNotifier
+notifier = EmailNotifier()
+arguments = {'Esperimento': f'sola raccomandazione su {dataset_name}'}
+notifier.notify(fun, additional_body=str(arguments))
